ef5712f26d992bc5ad3be66c0b493ca00383e465
cubajastk-hue <cubajastk@gmail.com>
2026-01-17 00:37:23 +0100

    Add files via upload

---
+++ tina/config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "tinacms";
+
+export default defineConfig({
+  branch: "main",
+  clientId: null, // Získáš později na tina.io
+  token: null,    // Získáš později na tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "./",
+  },
+  media: {
+    tina: {
+      mediaRoot: "uploads",
+      publicFolder: "./",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Domovská stránka",
+        path: "content",
+        format: "json",
+        fields: [
+          { type: "string", name: "title", label: "Nadpis webu" },
+          { type: "string", name: "description", label: "Popis podniku", ui: { component: "textarea" } },
+          { type: "image", name: "image", label: "Hlavní obrázek" },
+        ],
+      },
+    ],
+  },
+});
